0441affe5d5ba00a45742e256e0f905749d22fda
Jay Lee <jayleedev@hotmail.com>
2023-07-17 13:57:48 +0300

    remove folders

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,16 +1,14 @@
 import { Box } from '@mui/material';
-import useSignUpStyles from './signUpStyles';
 import Layout, { LayoutVariant } from '@/components/common/Layout';
 import SignUpForm from '@/components/page/SignUp/SignUpForm';
 import { useState } from 'react';
 import ConfirmEmail from '@/components/page/SignUp/ConfirmEmail';
 
 const SignUp = () => {
-  const classes = useSignUpStyles();
   const [email, setEmail] = useState('');
   return (
     <Layout variant={LayoutVariant.Entry}>
-      <Box sx={classes.root}>
+      <Box>
         {email ? (
           <ConfirmEmail email={email} />
         ) : (
