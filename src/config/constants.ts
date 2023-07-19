b55417351d9165d70c6e3c59ec75ad456e02958a
Jay Lee <jayleedev@hotmail.com>
2023-07-19 14:27:22 +0300

    create filter constants

---
+++ src/config/constants.ts
@@ -0,0 +1,13 @@
+export const STATUSES = [
+  { label: 'Open', value: 'open' },
+  { label: 'Closed - Active', value: 'closed' },
+  { label: 'Full cycle', value: 'full_cycle' },
+];
+
+export const REGIONS = [
+  { id: 1, label: 'Midwest', value: 'midwest', href: '#' },
+  { id: 2, label: 'Northwest', value: 'northwest', href: '#' },
+  { id: 3, label: 'Northeast', value: 'northeast', href: '#' },
+  { id: 4, label: 'Southeast', value: 'southeast', href: '#' },
+  { id: 5, label: 'Southwest', value: 'southwest', href: '#' },
+];
