bd869be16e5c2e044c83e1fe03eb1105f211ab7a
Jay Lee <jayleedev@hotmail.com>
2023-07-19 10:49:16 +0300

    create custom accordion

---
+++ src/components/common/Accordion.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useAccordionStyles } from './styles';
+
+interface CustomAccordionProps {
+  children: ReactNode;
+  label: string;
+}
+
+const CustomAccordion = ({ children, label }: CustomAccordionProps) => {
+  const classes = useAccordionStyles();
+  return (
+    <Accordion sx={classes.root}>
+      <AccordionSummary expandIcon={<KeyboardArrowDownIcon />}>
+        <Typography variant="body1">{label}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default CustomAccordion;
