4a2de76378811b4b24c602f42a6c8c8e218ff312
Jay Lee <jayleedev@hotmail.com>
2023-07-19 14:46:00 +0300

    separate filters in another file

--- src/pages/deals.tsx
+++ src/pages/deals.tsx
@@ -1,21 +1,11 @@
-import CustomAccordion from '@/components/common/Accordion';
-import CustomCheckbox from '@/components/common/CustomCheckbox';
 import DealCard, { DealCardVariant } from '@/components/common/DealCard';
 import Layout from '@/components/common/Layout';
-import { assetClasses } from '@/components/common/Layout/Header';
 import CustomSelect, { SelectVariant } from '@/components/common/Select';
-import { STATUSES } from '@/config/constants';
+import DealsFilters from '@/components/page/Deals/DealsFilters';
 import useHeaderProps from '@/hooks/useHeaderProps';
 import useDealsPageStyles from '@/pages_styles/dealsStyles';
 import { IDeal } from '@/types/deal';
 import { Box, Typography } from '@mui/material';
-import { useState } from 'react';
-
-interface IFilters {
-  ratings: number[];
-  asset_classes: string[];
-  statuses: string[];
-}
 
 const mockData: IDeal[] = [
   {
@@ -85,8 +75,6 @@ const sortOptions = [
   { label: 'Investment', value: 'investment' },
 ];
 
-const ratings = [5, 4, 3];
-
 const Deals = () => {
   const classes = useDealsPageStyles();
 
@@ -97,133 +85,11 @@ const Deals = () => {
     isSearch: true,
   });
 
-  const [filters, setFilters] = useState<IFilters>({
-    ratings: [],
-    asset_classes: [],
-    statuses: [],
-  });
-  const [showAll, setShowAll] = useState({
-    asset_classes: false,
-    sec_industry: false,
-  });
-
-  //rating
-  const handleRatingChange = (value: number) => {
-    if (filters.ratings.includes(value)) {
-      const ratings = filters.ratings.filter(item => item !== value);
-      setFilters({ ...filters, ratings });
-    } else {
-      const ratings = [...filters.ratings, value];
-      setFilters({ ...filters, ratings });
-    }
-  };
-
-  //asset_classes
-  const handleShowMoreAssetClasses = () => {
-    if (showAll.asset_classes) {
-      setShowAll({ ...showAll, asset_classes: false });
-    } else {
-      setShowAll({ ...showAll, asset_classes: true });
-    }
-  };
-  const assetClassesToShow = showAll.asset_classes
-    ? assetClasses
-    : assetClasses.slice(0, 8);
-  const handleAssetClassChange = (value: string) => {
-    if (filters.asset_classes.includes(value)) {
-      const asset_classes = filters.asset_classes.filter(
-        item => item !== value
-      );
-      setFilters({ ...filters, asset_classes });
-    } else {
-      const asset_classes = [...filters.asset_classes, value];
-      setFilters({ ...filters, asset_classes });
-    }
-  };
-
-  //statuses
-  const handleStatusesChange = (value: string) => {
-    if (filters.statuses.includes(value)) {
-      const statuses = filters.statuses.filter(item => item !== value);
-      setFilters({ ...filters, statuses });
-    } else {
-      const statuses = [...filters.statuses, value];
-      setFilters({ ...filters, statuses });
-    }
-  };
-
   return (
     <Layout {...headerProps}>
       <Box sx={classes.root}>
         <Box sx={classes.leftColumn}>
-          <CustomAccordion label="Sponsor Rating">
-            <Box sx={classes.accordionContent}>
-              {ratings.map(rating => (
-                <CustomCheckbox
-                  customStyles={classes.ratingCheckbox}
-                  key={rating}
-                  onChange={() => handleRatingChange(rating)}
-                  checked={filters.ratings.includes(rating)}
-                  label={
-                    <Box sx={classes.starsWrapper}>
-                      {[...Array(5)].map((_, i) => {
-                        const iconColor = i < rating ? '#F58F29' : '#DBE4F2';
-                        return (
-                          <i
-                            key={i}
-                            style={{ color: iconColor }}
-                            className="icon-Star"
-                          ></i>
-                        );
-                      })}
-                    </Box>
-                  }
-                />
-              ))}
-            </Box>
-          </CustomAccordion>
-          <CustomAccordion label="Asset Class">
-            <Box sx={classes.accordionContent}>
-              <Box sx={classes.assetClassesWrapper}>
-                {assetClassesToShow.map(assetClass => (
-                  <CustomCheckbox
-                    customStyles={classes.ratingCheckbox}
-                    key={assetClass.value}
-                    onChange={() => handleAssetClassChange(assetClass.value)}
-                    checked={filters.asset_classes.includes(assetClass.value)}
-                    label={assetClass.value}
-                  />
-                ))}
-              </Box>
-              <Typography
-                variant="body1"
-                sx={classes.showMore}
-                onClick={handleShowMoreAssetClasses}
-              >
-                {showAll.asset_classes ? 'Hide' : 'Show more'}{' '}
-                <span
-                  className={`icon-Caret-down ${
-                    showAll.asset_classes ? 'rotate' : ''
-                  }`}
-                ></span>
-              </Typography>
-            </Box>
-          </CustomAccordion>
-          <CustomAccordion label="Status">
-            <Box sx={classes.accordionContent}>
-              <Box sx={classes.assetClassesWrapper}>
-                {STATUSES.map(status => (
-                  <CustomCheckbox
-                    customStyles={classes.ratingCheckbox}
-                    key={status.value}
-                    onChange={() => handleStatusesChange(status.value)}
-                    checked={filters.statuses.includes(status.value)}
-                    label={status.label}
-                  />
-                ))}
-              </Box>
-            </Box>
-          </CustomAccordion>
+          <DealsFilters />
         </Box>
         <Box sx={classes.rightColumn}>
           <Box sx={classes.rightColumnHeader}>
--- src/pages_styles/dealsStyles.ts
+++ src/pages_styles/dealsStyles.ts
@@ -51,35 +51,6 @@ const useDealsPageStyles = () => {
       flexDirection: 'column',
       gap: '16px',
     },
-    accordionContent: {
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    starsWrapper: {
-      display: 'flex',
-      gap: '4px',
-    },
-    ratingCheckbox: {
-      padding: '4px 0px',
-    },
-    assetClassesWrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '4px',
-      marginBottom: '12px',
-    },
-    showMore: {
-      fontWeight: 600,
-      color: palette.primary.light,
-      padding: '4px 16px',
-      display: 'flex',
-      alignItems: 'center',
-      gap: '8px',
-      cursor: 'pointer',
-      '& span': {
-        fontSize: '24px',
-      },
-    },
   };
 };
 
