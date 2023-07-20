4fe863a0640df70304d20abe896497a7dd0dbcf1
Danil Vovchenko <tuborghorror@gmail.com>
2023-07-20 11:39:22 +0300

    Added create-deal, deal, get deals interfaces

---
+++ src/backend/services/deals/interfaces/get-all-deals.interface.ts
@@ -0,0 +1,24 @@
+import { AssetClasses } from '../../../constants/enums/asset-classes';
+import { OrderDirectionConstants } from '../../../constants/order-direction-constants';
+import { Regions } from '../../../constants/enums/regions';
+import { DealStatuses } from '../../../constants/enums/deal-statuses';
+import { InvestmentStructures } from '../../../constants/enums/investment-structures';
+import { Exemptions } from '../../../constants/enums/exemptions';
+
+export interface FindAllDealsInterface {
+  pageSize?: number;
+  page?: number;
+  order?: string;
+  orderDirection?: OrderDirectionConstants;
+  assetClasses?: AssetClasses[];
+  statuses?: DealStatuses[];
+  regions?: Regions[];
+  investmentStructures?: InvestmentStructures[];
+  IRRMin?: number;
+  IRRMax?: number;
+  investmentMinValue?: number;
+  investmentMaxValue?: number;
+  exemptions?: Exemptions[];
+  sponsorFeesMin?: number;
+  sponsorFeesMax?: number
+}
