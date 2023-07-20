25f2d43cea587e76706fc1c998571d69f23a4339
Jay Lee <jayleedev@hotmail.com>
2023-07-20 17:16:29 +0300

    add assetclasses enum

--- src/components/page/Home/DealsBlock.tsx
+++ src/components/page/Home/DealsBlock.tsx
@@ -2,28 +2,7 @@ import { Box, Typography, Grid } from '@mui/material';
 import Link from 'next/link';
 import { blueTitleStyles, useDealsBlockStyles } from './styles';
 import { Regions } from '@/backend/constants/enums/regions';
-
-const AssetClasses = [
-  { id: 1, name: 'Build-to-Rent', href: '#' },
-  { id: 2, name: 'Co-Living', href: '#' },
-  { id: 3, name: 'Data Center', href: '#' },
-  { id: 4, name: 'Flex R&D', href: '#' },
-  { id: 5, name: 'Flex/Office', href: '#' },
-  { id: 6, name: 'Hospitality', href: '#' },
-  { id: 7, name: 'Industrial', href: '#' },
-  { id: 8, name: 'Land Manufactured Housing', href: '#' },
-  { id: 9, name: 'Medical Office', href: '#' },
-  { id: 10, name: 'Mixed Use', href: '#' },
-  { id: 11, name: 'Multi-Asset', href: '#' },
-  { id: 12, name: 'Multifamily', href: '#' },
-  { id: 13, name: 'Office', href: '#' },
-  { id: 14, name: 'Parking Garage', href: '#' },
-  { id: 15, name: 'Retail', href: '#' },
-  { id: 16, name: 'Senior Housing', href: '#' },
-  { id: 17, name: 'Single Family', href: '#' },
-  { id: 18, name: 'Specialty', href: '#' },
-  { id: 19, name: 'Storage', href: '#' },
-];
+import { AssetClasses } from '@/backend/constants/enums/asset-classes';
 
 const DealsBlock = () => {
   const classes = useDealsBlockStyles();
@@ -31,6 +10,13 @@ const DealsBlock = () => {
     value: value,
     href: value.replace(/\s+/g, '_').toLowerCase(),
   }));
+  const assetClassesArray = [
+    ...Object.keys(AssetClasses).map(key => {
+      const value = AssetClasses[key as keyof typeof AssetClasses];
+      const href = value.replace(/[\s']/g, '_').toLowerCase();
+      return { value, href };
+    }),
+  ];
   return (
     <Box sx={classes.root}>
       <Typography variant="caption" sx={blueTitleStyles}>
@@ -45,10 +31,10 @@ const DealsBlock = () => {
             Asset Class
           </Typography>
           <Box sx={classes.list}>
-            {AssetClasses.map(asset => (
+            {assetClassesArray.map(asset => (
               <Link href={asset.href} key={asset.id}>
                 <Typography variant="body1">
-                  {asset.name}
+                  {asset.value}
                   <i className="icon-Caret-right"></i>
                 </Typography>
               </Link>
