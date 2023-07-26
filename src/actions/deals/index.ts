7d336b0c8dd07d41b02143b9775543e530951cf6
Jay Lee <jayleedev@hotmail.com>
2023-07-26 17:41:52 +0300

    rewrite to query-string

--- src/actions/deals/index.ts
+++ src/actions/deals/index.ts
@@ -1,6 +1,7 @@
 import { DealInterface } from '@/backend/services/deals/interfaces/deal.interface';
 import { IFilters } from '@/components/page/Deals/DealsFilters';
 import api from '@/config/ky';
+import queryString from 'query-string';
 
 interface IDealFilters extends IFilters {
   page: number;
@@ -17,55 +18,30 @@ export interface GetAllDealsResponse {
 export const getAllDeals = async (
   filters: IDealFilters
 ): Promise<GetAllDealsResponse> => {
-  const assetClasses = filters.asset_classes
-    ?.map(ac => `assetClasses=${ac}`)
-    .join('&');
-  const statuses = filters.statuses
-    ?.map(status => `statuses=${status}`)
-    .join('&');
-  const regions = filters.regions?.map(region => `regions=${region}`).join('&');
-  const investmentStructures = filters.investment_structure
-    ?.map(structure => `investmentStructures=${structure}`)
-    .join('&');
-  const exemptions = filters.exemptions
-    ?.map(exemption => `exemption=${exemption}`)
-    .join('&');
+  const parameters = {
+    page: filters.page,
+    pageSize: filters.pageSize,
+    orderDirection: 'ASC',
+    assetClasses: filters.asset_classes,
+    statuses: filters.statuses,
+    regions: filters.regions,
+    investmentStructures: filters.investment_structure,
+    exemption: filters.exemptions,
+    IRRMin: filters.targetIRR?.from,
+    IRRMax: filters.targetIRR?.to,
+    investmentMinValue: filters.min_investment?.from,
+    investmentMaxValue: filters.min_investment?.to,
+    sponsorFeesMin: filters.fees?.from,
+    sponsorFeesMax: filters.fees?.to,
+  };
 
-  const IRRMin = filters.targetIRR?.from
-    ? `IRRMin=${filters.targetIRR.from}`
-    : '';
-  const IRRMax = filters.targetIRR?.to ? `IRRMax=${filters.targetIRR.to}` : '';
-  const investmentMinValue = filters.min_investment?.from
-    ? `investmentMinValue=${filters.min_investment.from}`
-    : '';
-  const investmentMaxValue = filters.min_investment?.to
-    ? `investmentMaxValue=${filters.min_investment.to}`
-    : '';
-  const sponsorFeesMin = filters.fees?.from
-    ? `sponsorFeesMin=${filters.fees.from}`
-    : '';
-  const sponsorFeesMax = filters.fees?.to
-    ? `sponsorFeesMax=${filters.fees.to}`
-    : '';
+  const stringifiedParameters = queryString.stringify(parameters, {
+    arrayFormat: 'none',
+    skipNull: true,
+    skipEmptyString: true,
+  });
 
-  const params = [
-    `page=${filters.page}`,
-    `pageSize=${filters.pageSize}`,
-    'orderDirection=ASC',
-    assetClasses,
-    statuses,
-    regions,
-    investmentStructures,
-    IRRMin,
-    IRRMax,
-    investmentMinValue,
-    investmentMaxValue,
-    sponsorFeesMin,
-    sponsorFeesMax,
-    exemptions,
-  ];
-
-  const url = `deals?${params.filter(Boolean).join('&')}`;
+  const url = `deals?${stringifiedParameters}`;
 
   try {
     const response: GetAllDealsResponse = await api.get(url).json();
