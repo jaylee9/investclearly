2d0393342dd8a4ab44167cecac2d7b614d17ad89
Jay Lee <jayleedev@hotmail.com>
2023-07-27 15:45:12 +0300

    start to create sponsors filter

---
+++ src/components/page/List/Sponsors/SponsorsFilters.tsx
@@ -0,0 +1,245 @@
+import CustomAccordion from '@/components/common/Accordion';
+import CustomCheckbox from '@/components/common/CustomCheckbox';
+import { RATINGS } from '@/config/constants';
+import { Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useDealsFiltersStyles } from './styles';
+import CustomSlider from '@/components/common/Slider';
+import { Regions } from '@/backend/constants/enums/regions';
+import { InvestmentStructures } from '@/backend/constants/enums/investment-structures';
+import { DealStatuses } from '@/backend/constants/enums/deal-statuses';
+import { AssetClasses } from '@/backend/constants/enums/asset-classes';
+import { Exemptions } from '@/backend/constants/enums/exemptions';
+import Button from '@/components/common/Button';
+
+export interface IFilters {
+  ratings?: number[];
+  primaryAssetClasses?: string[];
+  regionalFocus?: string[];
+  activelyRaising?: boolean;
+}
+
+interface DealsFiltersProps {
+  setFilters: React.Dispatch<React.SetStateAction<IFilters>>;
+  filters: IFilters;
+  handleApplyFilters: () => void;
+}
+
+const SponsorsFilters = ({
+  setFilters,
+  filters,
+  handleApplyFilters,
+}: DealsFiltersProps) => {
+  const classes = useDealsFiltersStyles();
+
+  const [showAll, setShowAll] = useState({
+    asset_classes: false,
+    sec_industry: false,
+  });
+
+  const handleStringArrayChange = (
+    key: 'primaryAssetClasses' | 'regionalFocus',
+    value: Regions | string
+  ) => {
+    const updatedArray = filters[key]?.includes(value)
+      ? filters[key]?.filter(item => item !== value)
+      : [...(filters[key] || []), value];
+
+    setFilters({ ...filters, [key]: updatedArray });
+  };
+
+  //rating
+  const handleRatingChange = (value: number) => {
+    if (filters.ratings?.includes(value)) {
+      const ratings = filters.ratings.filter(item => item !== value);
+      setFilters({ ...filters, ratings });
+    } else if (filters.ratings) {
+      const ratings = [...filters.ratings, value];
+      setFilters({ ...filters, ratings });
+    }
+  };
+
+  //asset_classes
+  const handleShowMoreAssetClasses = () => {
+    if (showAll.asset_classes) {
+      setShowAll({ ...showAll, asset_classes: false });
+    } else {
+      setShowAll({ ...showAll, asset_classes: true });
+    }
+  };
+
+  const assetClassesToShow = showAll.asset_classes
+    ? Object.values(AssetClasses)
+    : Object.values(AssetClasses).slice(0, 8);
+
+  return (
+    <Box>
+      <CustomAccordion label="Sponsor Rating">
+        <Box sx={classes.accordionContent}>
+          {RATINGS.map(rating => (
+            <CustomCheckbox
+              customStyles={classes.ratingCheckbox}
+              key={rating}
+              onChange={() => handleRatingChange(rating)}
+              checked={filters.ratings?.includes(rating)}
+              label={
+                <Box sx={classes.starsWrapper}>
+                  {[...Array(5)].map((_, i) => {
+                    const iconColor = i < rating ? '#F58F29' : '#DBE4F2';
+                    return (
+                      <i
+                        key={i}
+                        style={{ color: iconColor }}
+                        className="icon-Star"
+                      ></i>
+                    );
+                  })}
+                </Box>
+              }
+            />
+          ))}
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Asset Class">
+        <Box sx={classes.accordionContent}>
+          <Box sx={classes.assetClassesWrapper}>
+            {assetClassesToShow.map(assetClass => (
+              <CustomCheckbox
+                customStyles={classes.ratingCheckbox}
+                key={assetClass}
+                onChange={() =>
+                  handleStringArrayChange('primaryAssetClasses', assetClass)
+                }
+                checked={filters.primaryAssetClasses?.includes(assetClass)}
+                label={assetClass}
+              />
+            ))}
+          </Box>
+          <Typography
+            variant="body1"
+            sx={classes.showMore}
+            onClick={handleShowMoreAssetClasses}
+          >
+            {showAll.asset_classes ? 'Hide' : 'Show more'}{' '}
+            <span
+              className={`icon-Caret-down ${
+                showAll.asset_classes ? 'rotate' : ''
+              }`}
+            ></span>
+          </Typography>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Investment Structure">
+        <Box sx={classes.accordionContent}>
+          <Box sx={classes.assetClassesWrapper}>
+            {Object.values(InvestmentStructures).map(structure => (
+              <CustomCheckbox
+                customStyles={classes.ratingCheckbox}
+                key={structure}
+                onChange={() =>
+                  handleStringArrayChange('investment_structure', structure)
+                }
+                checked={filters.investment_structure?.includes(structure)}
+                label={structure}
+              />
+            ))}
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Region">
+        <Box sx={classes.accordionContent}>
+          <Box sx={classes.assetClassesWrapper}>
+            {Object.values(Regions).map(region => (
+              <CustomCheckbox
+                customStyles={classes.ratingCheckbox}
+                key={region}
+                onChange={() => handleStringArrayChange('regions', region)}
+                checked={filters.regions?.includes(region)}
+                label={region}
+              />
+            ))}
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Exemption">
+        <Box sx={classes.accordionContent}>
+          <Box sx={classes.assetClassesWrapper}>
+            {Object.values(Exemptions).map(exemption => (
+              <CustomCheckbox
+                customStyles={classes.ratingCheckbox}
+                key={exemption}
+                onChange={() =>
+                  handleStringArrayChange('exemptions', exemption)
+                }
+                checked={filters.investment_structure?.includes(exemption)}
+                label={exemption}
+              />
+            ))}
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Target IRR, %">
+        <Box sx={classes.accordionContent}>
+          <Box>
+            <CustomSlider min={2} max={12} />
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Actual IRR, %">
+        <Box sx={classes.accordionContent}>
+          <Box>
+            <CustomSlider
+              min={2}
+              max={12}
+              onChange={value =>
+                handleSliderChange(value as number[], 'actualIRR')
+              }
+            />
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Fees, %">
+        <Box sx={classes.accordionContent}>
+          <Box>
+            <CustomSlider
+              min={2}
+              max={12}
+              onChange={value => handleSliderChange(value as number[], 'fees')}
+            />
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Minimum Investment, USD">
+        <Box sx={classes.accordionContent}>
+          <Box>
+            <CustomSlider
+              min={5000}
+              max={25000}
+              onChange={value =>
+                handleSliderChange(value as number[], 'min_investment')
+              }
+            />
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <CustomAccordion label="Preffered Return, USD">
+        <Box sx={classes.accordionContent}>
+          <Box>
+            <CustomSlider
+              min={5000}
+              max={25000}
+              onChange={value =>
+                handleSliderChange(value as number[], 'preffered_return')
+              }
+            />
+          </Box>
+        </Box>
+      </CustomAccordion>
+      <Box sx={classes.buttonWrapper}>
+        <Button onClick={handleApplyFilters}>Apply filters</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default SponsorsFilters;
