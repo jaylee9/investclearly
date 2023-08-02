46ac3bb448b6f05d889a3626418a33a0f8dc435b
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-02 17:38:59 +0300

    Changed update review logic

--- src/backend/services/reviews/update-review.ts
+++ src/backend/services/reviews/update-review.ts
@@ -5,12 +5,14 @@ import { getAttachments } from '../attachments/get-attachments';
 import { deleteFile } from '../files/delete-file';
 import { deleteAttachment } from '../attachments/delete-attachments';
 import { transformObjectKeysToArrays } from '../../utils/transform-object-keys-to-arrays';
-import { UpdateReviewInterface } from './interfaces/update-review.interface';
+import { UploadReviewProofsInterface } from './interfaces/upload-review-proofs.interface';
 import { getReviewById } from './get-review-by-id';
+import { getDatabaseConnection } from '../../../backend/config/data-source-config';
+import { Review } from '../../../backend/entities/reviews.entity';
 
 export const update = async (
   id: number,
-  fields: UpdateReviewInterface,
+  fields: UploadReviewProofsInterface,
   files: Express.Multer.File[]
 ) => {
   const { attachmentsIdsToDelete } = fields;
@@ -38,6 +40,9 @@ export const update = async (
       await deleteFile(attachment.path);
       await deleteAttachment(attachment.id);
     }
+
+    const connection = await getDatabaseConnection();
+    await connection.manager.update(Review, { id }, { isVerified: true });
   }
   return getReviewById(id);
 };
