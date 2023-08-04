22237efdb73d87be56c8c34bcf7da9b45039f462
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-04 18:35:36 +0300

    In getSponsorById added dealsCount, reviewsCount, average rating

--- src/backend/services/sponsors/get-sponsor-by-id.ts
+++ src/backend/services/sponsors/get-sponsor-by-id.ts
@@ -3,17 +3,48 @@ import { getDatabaseConnection } from '../../config/data-source-config';
 import { SponsorConstants } from '../../../backend/constants/validation/sponsor-constants';
 import { Sponsor } from '../../../backend/entities/sponsors.entity';
 import { sponsorMapper } from '../../../backend/mappers/sponsor.mapper';
+import { Deal } from '../../../backend/entities/deals.entity';
+import { Review } from '../../../backend/entities/reviews.entity';
+import { ReviewStatuses } from '../../../backend/constants/enums/review-statuses';
 
 export const getSponsorById = async (id: number) => {
   const connection = await getDatabaseConnection();
 
   const sponsor = await connection.manager.findOne(Sponsor, {
-    where: { id },
+    where: { id, reviews: { status: ReviewStatuses.published } },
     relations: ['user', 'deals', 'reviews'],
   });
 
   if (!sponsor) {
     throw new createHttpError.NotFound(SponsorConstants.sponsorNotFound);
+  }
+
+  sponsor.dealsCount = await connection.manager.count(Deal, {
+    where: { sponsorId: sponsor.id },
+  });
+
+  sponsor.reviewsCount = await connection.manager.count(Review, {
+    where: { sponsorId: sponsor.id, status: ReviewStatuses.published },
+  });
+
+  if (sponsor.reviews && sponsor.reviews.length > 0) {
+    let totalRating = 0;
+    let publishedReviewsCount = 0;
+
+    sponsor.reviews.forEach(review => {
+      if (review.status === ReviewStatuses.published) {
+        totalRating += review.overallRating;
+        publishedReviewsCount++;
+      }
+    });
+
+    if (publishedReviewsCount > 0) {
+      sponsor.avgTotalRating = totalRating / publishedReviewsCount;
+    } else {
+      sponsor.avgTotalRating = 0;
+    }
+  } else {
+    sponsor.avgTotalRating = 0;
   }
 
   return sponsorMapper(sponsor);
