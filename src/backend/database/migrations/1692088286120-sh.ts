0d49cb3063a4be27c01c223a26ae4e52ed77a2d7
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-15 11:33:58 +0300

    Generated migration

---
+++ src/backend/database/migrations/1692088286120-sh.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Sh1692088286120 implements MigrationInterface {
+  name = 'Sh1692088286120';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."investments_status_enum" AS ENUM('active', 'completed')`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "investments" ("id" SERIAL NOT NULL, "deal_id" integer NOT NULL, "user_id" integer NOT NULL, "date_of_investment" date NOT NULL, "total_invested" integer NOT NULL, "status" "public"."investments_status_enum" NOT NULL DEFAULT 'active', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a1263853f1a4fb8b849c1c9aff4" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "investments" ADD CONSTRAINT "FK_fe9d6987f15c1cce3ff55dd25e2" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "investments" ADD CONSTRAINT "FK_d7df28d64beea1bdd93ec5639b1" FOREIGN KEY ("deal_id") REFERENCES "deals"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "investments" DROP CONSTRAINT "FK_d7df28d64beea1bdd93ec5639b1"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "investments" DROP CONSTRAINT "FK_fe9d6987f15c1cce3ff55dd25e2"`
+    );
+    await queryRunner.query(`DROP TABLE "investments"`);
+    await queryRunner.query(`DROP TYPE "public"."investments_status_enum"`);
+  }
+}
