51befd2f45ba22621d2c1667684ba1fcabdf4359
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-16 13:29:07 +0300

    Fix comments

--- src/pages/api/deals/[id].ts
+++ src/pages/api/deals/[id].ts
@@ -11,9 +11,7 @@ const getDeal = async (request: NextApiRequest, response: NextApiResponse) => {
   );
 
   const id: number = Number(authRequest.query.id);
-  const dealRecord = user
-    ? await getDealById(id, user.id)
-    : await getDealById(id);
+  const dealRecord = await getDealById(id, user?.id);
 
   response.status(200).json(dealRecord);
 };
