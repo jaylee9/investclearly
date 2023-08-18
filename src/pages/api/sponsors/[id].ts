acf9d87ac1dde2338d2adb25df8d1828c0046ff4
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-18 15:35:39 +0300

    Added optional token check to getSponsor and getSponsors api routes

--- src/pages/api/sponsors/[id].ts
+++ src/pages/api/sponsors/[id].ts
@@ -1,13 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { apiHandler } from '../../../backend/utils/api-handler';
 import { getSponsorById } from '../../../backend/services/sponsors/get-sponsor-by-id';
+import { authMiddleware } from '../../../backend/middleware/auth';
 
 const getSponsor = async (
   request: NextApiRequest,
   response: NextApiResponse
 ) => {
-  const id: number = Number(request.query.id);
-  const sponsorRecord = await getSponsorById(id);
+  const { request: authRequest, user } = await authMiddleware(
+    request,
+    response,
+    true
+  );
+  const currentUserId = user?.id;
+  const id: number = Number(authRequest.query.id);
+  const sponsorRecord = await getSponsorById(id, currentUserId);
   response.status(200).json(sponsorRecord);
 };
 
