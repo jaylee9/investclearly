ca8bab37b08ac4b4f287cf0e02b2eb7d4acc6e67
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-21 22:16:12 +0300

    Added defaultLimits to users constants

--- src/backend/constants/users-constants.ts
+++ src/backend/constants/users-constants.ts
@@ -2,4 +2,8 @@ export const UserConstants = {
   userWithSameUsernameExists: 'A user with the same username already exists',
   userNotFound: 'User not found',
   accountWasSuccessfullyDeactivated: 'Account was successfully deactivated',
+  defaultLimits: {
+    defaultReviewsLimit: 4,
+    defaultInvestmentsLimit: 3,
+  },
 };
