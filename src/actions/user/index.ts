5d466e46ef16e2299db497b898a410866a551d23
Jay Lee <jayleedev@hotmail.com>
2023-08-21 12:45:47 +0300

    add request on page

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -1,9 +1,9 @@
-import { UserInterface } from '@/backend/services/users/interfaces/user.interface';
+import { PublicUserInterface } from '@/backend/services/users/interfaces/public-user.interface';
 import api from '@/config/ky';
 
 export const getUser = async ({ id }: { id: string }) => {
   try {
-    const response: UserInterface = await api.get(`deals/${id}`).json();
+    const response: PublicUserInterface = await api.get(`deals/${id}`).json();
     return response;
   } catch (error) {
     console.error('Error fetching user', error);
--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,9 +1,22 @@
+import { getAllDeals } from '@/actions/deals';
+import { getUser } from '@/actions/user';
+import { DealInterface } from '@/backend/services/deals/interfaces/deal.interface';
+import { ReviewInterface } from '@/backend/services/reviews/interfaces/review.interface';
+import { PublicUserInterface } from '@/backend/services/users/interfaces/public-user.interface';
 import Layout from '@/components/common/Layout';
 import useHeaderProps from '@/hooks/useHeaderProps';
 import usePublicUserPageStyles from '@/pages_styles/publicUserPageStyles';
 import { Box } from '@mui/material';
+import { GetServerSideProps } from 'next';
+import { useState } from 'react';
 
-const PublicUserPage = () => {
+interface PublicUserPage {
+  user: PublicUserInterface;
+  reviews: ReviewInterface[];
+  deals: DealInterface[];
+}
+
+const PublicUserPage = ({ user, reviews, deals }) => {
   const headerProps = useHeaderProps({
     type: 'search-dark',
     isLinks: true,
@@ -12,6 +25,9 @@ const PublicUserPage = () => {
     isFixed: true,
   });
   const classes = usePublicUserPageStyles();
+
+  const [reviewsData] = useState(reviews);
+  const [dealsData] = useState(deals);
   return (
     <Layout {...headerProps}>
       <Box sx={classes.root}>
@@ -19,6 +35,25 @@ const PublicUserPage = () => {
       </Box>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const id = context.params?.id as string;
+  const userResponse = await getUser({ id });
+  //mock request that will be replaced by user deals
+  const deals = await getAllDeals({ page: 1, pageSize: 3 });
+  if (!userResponse) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      user: userResponse,
+      reviews: userResponse.reviews,
+      deals,
+    },
+  };
 };
 
 export default PublicUserPage;
