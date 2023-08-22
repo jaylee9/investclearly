5f8b0875f1e62ea01c40f9ef381c1909e9faaeca
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-22 17:50:24 +0300

    Extended moderate review service, added bmRecipientEmails logic

--- src/backend/services/reviews/moderate-review.ts
+++ src/backend/services/reviews/moderate-review.ts
@@ -1,3 +1,4 @@
+import { Not } from 'typeorm';
 import { Review } from '../../../backend/entities/reviews.entity';
 import { getDatabaseConnection } from '../../../backend/config/data-source-config';
 import { ReviewStatuses } from '../../../backend/constants/enums/review-statuses';
@@ -7,6 +8,10 @@ import { sendReviewPublishedEmail } from '../mails/send-review-published-email';
 import { sendUnverifiedReviewPublishedEmail } from '../mails/send-unverified-review-published-email';
 import { sendReviewUnpublishedEmail } from '../mails/send-review-unpublished-email';
 import { sendReviewRejectedEmail } from '../mails/send-review-rejected-email';
+import { Bookmark } from '../../../backend/entities/bookmark.entity';
+import { BookmarkConstants } from '../../../backend/constants/bookmark-constants';
+import { sendReviewPublishedEmailToBmRecipients } from '../mails/send-review-published-email-to-bm-recipients';
+import { sendUnverifiedReviewPublishedEmailToBmRecipients } from '../mails/send-unverified-review-published-email-to-bm-recipients';
 
 export const moderate = async (
   id: number,
@@ -16,6 +21,17 @@ export const moderate = async (
   const { status, reason, unpublishReviewMessage, rejectReviewMessage } =
     reviewData;
   const reviewRecord = await getReviewById(id);
+  const bookmarks = await connection.manager.find(Bookmark, {
+    where: {
+      entityId: reviewRecord.sponsorId,
+      entityType: BookmarkConstants.entityTypes.sponsor,
+      userId: Not(reviewRecord.reviewerId),
+    },
+    relations: ['user'],
+  });
+
+  const bmRecipientEmails = bookmarks.map(obj => obj.user.email);
+
   if (status === ReviewStatuses.published) {
     const isVerified = reviewRecord.attachments?.length !== 0;
     await connection.manager.update(
@@ -26,8 +42,20 @@ export const moderate = async (
 
     if (isVerified) {
       await sendReviewPublishedEmail(reviewRecord);
+      if (bmRecipientEmails?.length) {
+        await sendReviewPublishedEmailToBmRecipients(
+          bmRecipientEmails,
+          reviewRecord
+        );
+      }
     } else {
       await sendUnverifiedReviewPublishedEmail(reviewRecord);
+      if (bmRecipientEmails?.length) {
+        await sendUnverifiedReviewPublishedEmailToBmRecipients(
+          bmRecipientEmails,
+          reviewRecord
+        );
+      }
     }
   }
 
