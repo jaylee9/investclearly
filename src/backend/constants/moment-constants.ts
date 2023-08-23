b5ab0b855dcfb62fbf7b65bce069a505d466ddfe
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-23 21:26:12 +0300

    Extended mail constants

--- src/backend/constants/moment-constants.ts
+++ src/backend/constants/moment-constants.ts
@@ -1,3 +1,6 @@
 export const MomentConstants = {
   dateFormatForModerationReviews: 'MMM D, YYYY',
+  dateStart: 'YYYY-MM-DDT00:00:00.00',
+  dateEnd: 'YYYY-MM-DDT23:59:000',
+  yearMonthDay: 'YYYY-MM-DD',
 };
