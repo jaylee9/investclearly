0b8094c7d4d7ea17a4768b714917424358c7c0b6
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-28 19:00:10 +0300

    Fix comments, renamed services

--- src/backend/services/cron-services/send-mail-to-users-with-new-deals-from-bookmarked-sponsors.ts
+++ src/backend/services/cron-services/send-mail-to-users-with-new-deals-from-bookmarked-sponsors.ts
@@ -3,34 +3,40 @@ import { User } from '../../entities/user.entity';
 import { getDatabaseConnection } from '../../config/data-source-config';
 import { Deal } from '../../entities/deals.entity';
 import { MomentConstants } from '../../constants/moment-constants';
-import { sendDealsMatchedYourInvestmentPreferences } from '../mails/send-deals-mathing-your-investment-preferences';
 import { Attachment } from '../../entities/attachments.entity';
 import { TargetTypesConstants } from '../../constants/target-types-constants';
 import { dealMapper } from '../../mappers/deal.mapper';
-import { DealConstants } from '../../../backend/constants/deal-constants';
+import { BookmarkConstants } from '../../constants/bookmark-constants';
+import { sendSponsorPostedNewDeals } from '../mails/send-sponsor-posted-new-deals';
+import { DealConstants } from '../../constants/deal-constants';
 
-export const getUsersAndTheirMatchedInvestmentPreferencesDeals = async () => {
+export const sendMailToUsersWithNewDealsFromBookmarkedSponsors = async () => {
   const connection = await getDatabaseConnection();
 
   const users = await connection.manager
     .createQueryBuilder()
     .select('user')
     .from(User, 'user')
-    .where('user.newDealMathingYourInvestmentPreferencesEmail = :value', {
+    .where('user.newDealFromTheSponsorYouSavedEmail = :value', {
       value: true,
     })
+    .leftJoinAndSelect(
+      'user.bookmarks',
+      'bookmarks',
+      'bookmarks.entityType = :entityType',
+      { entityType: BookmarkConstants.entityTypes.sponsor }
+    )
     .getMany();
 
   for (const user of users) {
-    const assetClasses = user.assetClasses;
-    if (assetClasses?.length) {
+    if (user.bookmarks?.length) {
+      const sponsorIds = user.bookmarks.map(item => item.entityId);
       const fromDate =
         moment().subtract(6, 'days').format(MomentConstants.dateStart) + 'Z';
       const toDate = moment(fromDate, MomentConstants.yearMonthDay).add(
         6,
         'days'
       );
-
       const toDateEndOfTheDay =
         moment(toDate).format(MomentConstants.dateEnd) + 'Z';
 
@@ -46,22 +52,19 @@ export const getUsersAndTheirMatchedInvestmentPreferencesDeals = async () => {
           { entityType: TargetTypesConstants.deals }
         )
         .leftJoinAndSelect('deals.sponsor', 'sponsor')
-        .where('deals.assetClass IN (:...assetClasses)', {
-          assetClasses,
-        })
+        .andWhere('sponsor.id IN (:...sponsorIds)', { sponsorIds })
         .andWhere(
           'deals.createdAt <= :toDateEndOfTheDay AND deals.createdAt >= :fromDate',
           { fromDate, toDateEndOfTheDay }
         )
-        .limit(DealConstants.maxAmountOfDealsMatchedOnUserInvestmentPreferences)
+        .limit(DealConstants.maxAmountNewDealsAtBookmarkedSponsor)
         .getMany();
 
       if (
         user &&
-        deals?.length ===
-          DealConstants.maxAmountOfDealsMatchedOnUserInvestmentPreferences
+        deals?.length === DealConstants.maxAmountNewDealsAtBookmarkedSponsor
       ) {
-        sendDealsMatchedYourInvestmentPreferences(
+        sendSponsorPostedNewDeals(
           user,
           await Promise.all(deals.map(dealMapper))
         );
