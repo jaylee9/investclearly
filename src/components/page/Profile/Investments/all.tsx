35838c1ac5157c0dcac5dd01a3ab4e43fa710be2
Jay Lee <jayleedev@hotmail.com>
2023-08-30 22:18:58 +0300

    remove state and console log

--- src/components/page/Profile/Investments/all.tsx
+++ src/components/page/Profile/Investments/all.tsx
@@ -12,17 +12,7 @@ import Loading from '@/components/common/Loading';
 const AllInvestments = () => {
   const classes = useAllInvestmentsStyles();
   const [page] = useState(1);
-  const [investmentsData, setInvestmentsData] =
-    useState<GetAllInvestmentsResponse>({
-      deals: [],
-      total: 0,
-      totalInvested: 0,
-      nextPage: 0,
-      currentPage: page,
-      prevPage: 0,
-      lastPage: 0,
-    });
-  const { isLoading } = useQuery(
+  const { data, isLoading } = useQuery(
     ['allInvestments'],
     () =>
       getAllInvestments({
@@ -31,24 +21,20 @@ const AllInvestments = () => {
         orderDirection: OrderDirectionConstants.DESC,
       }),
     {
-      onSuccess: data => {
-        setInvestmentsData(data);
-      },
       keepPreviousData: true,
     }
   );
-  console.log(isLoading);
   return (
     <Box sx={classes.root}>
       {isLoading && <Loading />}
-      {!isLoading && investmentsData && (
+      {!isLoading && (
         <Box>
           <Box marginBottom="24px">
             <Typography variant="caption" sx={classes.title}>
               Total Invested
             </Typography>
             <Typography variant="h2" fontWeight={600}>
-              ${investmentsData?.totalInvested}
+              ${data?.totalInvested}
             </Typography>
           </Box>
         </Box>
