72319e3744e2fb18a7e07d47a9904af2b067f640
Danil Vovchenko <tuborghorror@gmail.com>
2023-08-30 22:36:12 +0300

    Hot fix

--- src/backend/database/migrations/1693409937472-sh.ts
+++ src/backend/database/migrations/1693409937472-sh.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Sh1693409937472 implements MigrationInterface {
-    name = 'Sh1693409937472'
+  name = 'Sh1693409937472';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "deals" ADD "preferred_return" integer`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "deals" ADD "preferred_return" integer`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "deals" DROP COLUMN "preferred_return"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "deals" DROP COLUMN "preferred_return"`
+    );
+  }
 }
