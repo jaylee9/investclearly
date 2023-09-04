2c298265216d37f7c17db48d1fcaf04492583862
Jay Lee <jayleedev@hotmail.com>
2023-09-04 13:39:02 +0300

    add tabs

--- src/components/page/Profile/Reviews/index.tsx
+++ src/components/page/Profile/Reviews/index.tsx
@@ -1,7 +1,32 @@
 import { Box } from '@mui/material';
+import { useProfileReviewsStyles } from './styles';
+import { SyntheticEvent, useState } from 'react';
+import CustomTabs from '@/components/common/CustomTabs';
 
 const ProfileReviews = () => {
-  return <Box></Box>;
+  const classes = useProfileReviewsStyles();
+  const [activeTab, setActiveTab] = useState('published');
+  const handleChangeTab = (
+    event: SyntheticEvent<Element, Event>,
+    newValue: string | number
+  ) => {
+    setActiveTab(newValue as string);
+  };
+  const tabs = [
+    {
+      value: 'published',
+      label: 'Published',
+    },
+    {
+      value: 'on moderation',
+      label: 'On moderation',
+    },
+  ];
+  return (
+    <Box sx={classes.root}>
+      <CustomTabs value={activeTab} onChange={handleChangeTab} tabs={tabs} />
+    </Box>
+  );
 };
 
 export default ProfileReviews;
