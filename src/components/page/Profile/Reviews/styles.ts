d9b46d45633b3ef8beffb9859280daeb3ba5584c
Jay Lee <jayleedev@hotmail.com>
2023-09-05 13:43:36 +0300

    change logic for review card

--- src/components/page/Profile/Reviews/styles.ts
+++ src/components/page/Profile/Reviews/styles.ts
@@ -5,5 +5,27 @@ const { palette } = theme;
 export const useProfileReviewsStyles = () => {
   return {
     root: {},
+    warning: {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '8px',
+      color: palette.error.light,
+      padding: '8px',
+      borderRadius: '36px',
+      background: palette.error.contrastText,
+      width: '100%',
+      '& i': {
+        fontSize: '24px',
+      },
+    },
+    reviewsWrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '8px',
+      padding: '24px 0px',
+    },
+    showMore: {
+      color: palette.primary.light,
+    },
   };
 };
