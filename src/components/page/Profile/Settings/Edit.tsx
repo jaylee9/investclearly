76d8ec63b3eae8817fc743713ae606243ca783ff
Jay Lee <jayleedev@hotmail.com>
2023-09-11 15:53:44 +0300

    creating edit profile form

--- src/components/page/Profile/Settings/Edit.tsx
+++ src/components/page/Profile/Settings/Edit.tsx
@@ -9,12 +9,18 @@ import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import Input from '@/components/common/Input';
+import CustomSelect, { SelectVariant } from '@/components/common/Select';
+import { Regions } from '@/backend/constants/enums/regions';
 
 const isBrowser =
   typeof window !== 'undefined' && typeof window.File !== 'undefined';
 
 const validationSchema = z.object({
   profilePicture: isBrowser ? z.instanceof(File) : z.any(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  address: z.string().optional(),
 });
 
 type ValidationSchema = z.infer<typeof validationSchema>;
@@ -22,7 +28,6 @@ type ValidationSchema = z.infer<typeof validationSchema>;
 const EditProfile = () => {
   const classes = useEditProfileStyles();
   const [isLoading, setIsLoading] = useState(true);
-
   const [userId, setUserId] = useState('');
 
   useEffect(() => {
@@ -39,14 +44,27 @@ const EditProfile = () => {
         investmentsLimit: 1,
       }) as Promise<PublicUserInterface>;
     },
-    { enabled: !!userId, onSuccess: () => setIsLoading(false) }
+    {
+      enabled: !!userId,
+      onSuccess: data => {
+        setValue('firstName', data?.firstName || '');
+        setValue('lastName', data?.lastName || '');
+        setValue('address', data?.address || '');
+        setIsLoading(false);
+      },
+    }
   );
 
-  const { control, handleSubmit } = useForm<ValidationSchema>({
-    resolver: zodResolver(validationSchema),
-  });
+  const { control, handleSubmit, register, setValue, watch } =
+    useForm<ValidationSchema>({
+      resolver: zodResolver(validationSchema),
+    });
 
   const onSubmit = handleSubmit(data => console.log(data));
+
+  const regionsOptions = Object.values(Regions).map(item => {
+    return { label: item, value: item };
+  });
 
   return (
     <Box sx={classes.root}>
@@ -54,17 +72,47 @@ const EditProfile = () => {
         <Loading />
       ) : (
         <form onSubmit={onSubmit}>
-          <Controller
-            control={control}
-            name="profilePicture"
-            render={({ field: { onChange } }) => (
-              <ProfilePictureUploader
-                onChange={onChange}
-                defaultImage={data?.profilePicture}
-                username={`${data?.firstName} ${data?.lastName}`}
+          <Box sx={classes.uploaderWrapper}>
+            <Controller
+              control={control}
+              name="profilePicture"
+              render={({ field: { onChange } }) => (
+                <ProfilePictureUploader
+                  onChange={onChange}
+                  defaultImage={data?.profilePicture}
+                  username={`${data?.firstName} ${data?.lastName}`}
+                />
+              )}
+            />
+          </Box>
+          <Box sx={classes.content}>
+            <Box sx={classes.nameInputsWrapper}>
+              <Input
+                topLabel="First Name"
+                showClearOption={false}
+                register={register('firstName')}
+                value={watch('firstName')}
               />
-            )}
-          />
+              <Input
+                topLabel="Last Name"
+                showClearOption={false}
+                register={register('lastName')}
+                value={watch('lastName')}
+              />
+            </Box>
+            <Box sx={classes.singleInputsWrapper}>
+              <Input
+                topLabel="Address"
+                showClearOption={false}
+                register={register('address')}
+                value={watch('address')}
+              />
+              <CustomSelect
+                options={regionsOptions}
+                variant={SelectVariant.Dark}
+              />
+            </Box>
+          </Box>
         </form>
       )}
     </Box>
--- src/components/page/Profile/Settings/styles.ts
+++ src/components/page/Profile/Settings/styles.ts
@@ -3,5 +3,22 @@ export const useEditProfileStyles = () => {
     root: {
       padding: '24px',
     },
+    uploaderWrapper: {
+      marginBottom: '12px',
+    },
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '12px',
+      maxWidth: '600px',
+    },
+    nameInputsWrapper: {
+      display: 'flex',
+      gap: '12px',
+      width: '100%',
+    },
+    singleInputsWrapper: {
+      maxWidth: '560px',
+    },
   };
 };
