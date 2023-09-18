92785a7e0962d5c58e9099aeea2527158d1d0545
Danil Vovchenko <tuborghorror@gmail.com>
2023-09-18 16:52:34 +0300

    Updated deals and sponsors constants added range data constants

---
+++ src/backend/constants/range-data-constants.ts
@@ -0,0 +1,12 @@
+export const RangeDataConstants = {
+  minInvestment: 0,
+  maxInvestment: 0,
+  minTargetIRR: 0,
+  maxTargetIRR: 0,
+  minActualIRR: 0,
+  maxActualIRR: 0,
+  minFee: 0,
+  maxFee: 0,
+  minPreferredReturn: 0,
+  maxPreferredReturn: 0,
+};
