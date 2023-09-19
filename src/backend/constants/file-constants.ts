898e6d412a1f39546f326ebc473ea842f6aecf41
Danil Vovchenko <tuborghorror@gmail.com>
2023-09-19 17:09:55 +0300

    Created FileConstants, updated upload file service

---
+++ src/backend/constants/file-constants.ts
@@ -0,0 +1,9 @@
+export const FileConstants = {
+  contentTypes: {
+    applicationPdf: 'application/pdf',
+    applicationMsword: 'application/msword',
+  },
+  pdfExtension: '.pdf',
+  imageExtensions: ['.jpg', '.jpeg', '.gif'],
+  textFilesExtensions: ['.doc', '.docx'],
+};
