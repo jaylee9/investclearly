b38c639abce97382b77ae0dd664d433b910070ee
Danil Vovchenko <tuborghorror@gmail.com>
2023-09-28 22:07:05 +0300

    Updated prepareRelatedPersonData service

--- src/backend/services/sec-api-scraper/prepare-related-person-data.ts
+++ src/backend/services/sec-api-scraper/prepare-related-person-data.ts
@@ -10,8 +10,6 @@ export const prepareRelatedPersonData = async (offering: FormD) => {
       firstName: relatedPerson?.relatedPersonName?.firstName || '',
       middleName: relatedPerson?.relatedPersonName?.middleName || '',
       lastName: relatedPerson?.relatedPersonName?.lastName || '',
-      relationships:
-        relatedPerson?.relatedPersonRelationshipList?.relationship || [],
       relationshipClarification: relatedPerson?.relationshipClarification || '',
       location: relatedPerson?.relatedPersonAddress || '',
     };
@@ -20,7 +18,11 @@ export const prepareRelatedPersonData = async (offering: FormD) => {
     );
 
     if (relatedPersonRecord) {
-      relatedPersonRecords.push(relatedPersonRecord);
+      relatedPersonRecords.push({
+        ...relatedPersonRecord,
+        relatedPersonRoles:
+          relatedPerson?.relatedPersonRelationshipList?.relationship || [],
+      });
     }
   }
 
