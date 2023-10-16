b0f1e4c23cdf326d6892ca33f6438a21f1a4c3cc
Danil Vovchenko <tuborghorror@gmail.com>
2023-10-16 18:06:45 +0300

    Removed extra deal constants

--- src/backend/constants/deal-constants.ts
+++ src/backend/constants/deal-constants.ts
@@ -1,7 +1,5 @@
 export const DealConstants = {
   dealNotFound: 'Deal not found',
   dealSuccessfullyDeleted: 'Deal successfully deleted',
-  maxAmountOfDealsMatchedOnUserInvestmentPreferences: 3,
-  maxAmountNewDealsAtBookmarkedSponsor: 2,
   deals: 'deals',
 };
